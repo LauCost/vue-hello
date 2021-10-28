fb4670d454d3591f97bba8a7b1efc9407ab86071
Laurentiu <lcostin40@gmail.com>
2021-10-28 16:07:04 +0200

    Aggiunto testo e img attualmente non funzionante

---
+++ JS/mian.js
@@ -0,0 +1,10 @@
+/* Vue.config.devtools = true; */
+
+const vueApp = new Vue({
+    el: "#app",
+    data: {
+        message: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, vero in animi ad quis dignissimos quod quas, ",
+        className: "rosso",
+        img: "./img/1.jpg"
+    }
+});
